0fc40fa33fdbdd09ab5e2628217ed511b181cb9d
Julius Steck <Julius>
2023-05-25 17:38:21 +0200

    bug fix

--- helloWorld.php
+++ helloWorld.php
@@ -9,9 +9,38 @@
 
 <body>
 
-  <?php
-echo "Hello World!";
-?>
+    <?php
+      echo "Hello World!";
+    ?>
+
+    <?php
+      // Establish a database connection
+      $host = "localhost";
+      $database = "JuliusSteckWebserver";
+      $username = "ikrz2a6ngl7";
+      $password = "JuliusSteckWebserver#1";
+
+      $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+
+
+      $query = "SELECT EntryID, EntryTitle, EntryDate, EntryCover FROM Blog";
+      $statement = $pdo->query($query);
+
+
+      while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+          $entryID = $row['EntryID'];
+          $entryTitle = $row['EntryTitle'];
+          $entryDate = $row['EntryDate'];
+          $entryCover = $row['EntryCover'];
+
+
+          echo "<h2>$entryTitle</h2>";
+          echo "<p>Date: $entryDate</p>";
+          echo "<img src='data:image/jpeg;base64," . base64_encode($entryCover) . "' alt='Entry Cover'>";
+          echo "<hr>";
+      }
+    ?>
+
 
     <div class="container">
 
