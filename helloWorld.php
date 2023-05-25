165bbacb95eedf0ecfce506202c802052c3354ef
Julius Steck <Julius>
2023-05-25 17:26:16 +0200

    bug fix

---
+++ helloWorld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Julius Steck</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+
+  <?php
+echo "Hello World!";
+?>
+
+    <div class="container">
+
+    </div>
+</body>
+</html>
